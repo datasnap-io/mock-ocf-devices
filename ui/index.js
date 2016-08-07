2069102a2531bcbcde3bec617e97bfff96701315
Derek <derek.barnhart@neustar.biz>
2016-08-07 16:32:05 -0700

    add ui

---
+++ ui/index.js
@@ -0,0 +1,41 @@
+var http = require('http');
+var socketio = require('socket.io');
+var fs = require('fs');
+var ocfServer = require('ocf_server');
+
+var state = 'off';
+
+var server = http.createServer(function (request, response) {
+  response.writeHead(200, {"Content-Type": "text/html"});
+  fs.readFile( __dirname + '/index.html', function( err, data){
+    response.end(data.toString('utf-8'));
+  });
+});
+var io = socketio(server);
+server.listen(8090);
+
+console.log("Server running at http://127.0.0.1:8090/");
+
+ocfServer.onResource(function( event ){
+  console.log(event);
+});
+
+io.on('connection',function(socket){
+  console.log('Lightbulb listener connected')
+  socket.emit('update',{state:state})
+});
+
+function updateUI(state){
+  io.emit('update',{
+    state: state
+  });
+}
+
+// setInterval(function(){
+//   if(state === "off"){
+//     state = 'on'
+//   } else {
+//     state = 'off'
+//   }
+//   updateUI(state)
+// },5000)
---
+++ ui/ocf_server.js
@@ -0,0 +1,55 @@
+// Copyright 2016 Intel Corporation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+var device,
+	observationCount = 0;
+
+console.log( "Acquiring OCF device" );
+device = require( "iotivity-node" )( "client" );
+
+module.exports = {
+
+  onResource: device.addEventListener.bind(null, "resourcefound"),
+  discover: function(){
+    console.log( "Issuing discovery request" );
+    device.findResources().catch( function( error ) {
+    	console.error( error.stack ? error.stack : ( error.message ? error.message : error ) );
+    	//process.exit( 1 );
+    } );
+  }
+}
+
+
+// Add a listener that will receive the results of the discovery
+// device.addEventListener( "resourcefound", function( event ) {
+// 	console.log( "Discovered resource(s) via the following event:\n" +
+// 		JSON.stringify( event, null, 4 ) );
+//
+// 	// We've discovered the resource we were seeking.
+// 	if ( event.resource.id.path === "/a/high-level-example" ) {
+// 		var resourceUpdate = function( event ) {
+// 			console.log( "Received resource update event:\n" +
+// 				JSON.stringify( event, null, 4 ) );
+//
+// 			// Stop observing after having made 10 observations
+// 			if ( ++observationCount >= 10 ) {
+// 				event.resource.removeEventListener( "change", resourceUpdate );
+// 			}
+// 		};
+//
+// 		console.log( "This is the resource we want to observe" );
+//
+// 		// Let's start observing the resource.
+// 		event.resource.addEventListener( "change", resourceUpdate );
+// 	}
+// } );
